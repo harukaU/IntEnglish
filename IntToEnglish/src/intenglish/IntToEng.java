b126e7c71fa2c66b430b201fcffa41213b2d9360
haruka <haruka@localhost>
2014-05-14 13:26:09 +0900

    テストケース作成中です。 ドライバー：G12959 宇都宮遥 ナビゲーター：G12934 中村美穂

--- IntToEnglish/src/intenglish/IntToEng.java
+++ IntToEnglish/src/intenglish/IntToEng.java
@@ -3,7 +3,7 @@ package intenglish;
 import java.util.Scanner;
 
 public class IntToEng {
-    public static void main(String[] args) {
+    public void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
         
@@ -12,7 +12,7 @@ public class IntToEng {
     }
     
     // 数値を英訳する変換するメソッド
-    static String translateEng(int n) {
+    public String translateEng(int n) {
     	String[] num1={"zero", "one", "two", "three", "four", "five", "six", "seven","eight","nine","ten", "eleven","twelve","thirteen", "fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
         String[] num2={"twenty", "thirty", "forty","fifty","sixty","seventy","eighty","ninety"};
         String number;
--- IntToEnglish/test/intenglish/IntToEnglishTest.java
+++ IntToEnglish/test/intenglish/IntToEnglishTest.java
@@ -1,14 +1,20 @@
 package intenglish;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatcher.is;
 
 import org.junit.Test;
+
 
 public class IntToEnglishTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() {
+		IntToEng ie=new IntToEng();
+		String expected="one";
+		String actual=ie.translateEng(1);
+		assertThat(actual, is(expected));
 	}
+
 
 }
